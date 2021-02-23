7815bff6e3db42f1b8087af829ae10175786da5a
anas11cs <annus.waheed01@gmail.com>
2021-02-23 21:58:19 +0500

    Working Progress on RenderMessages component inside ChatFeed component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,7 +7,34 @@ const ChatFeed = (props) =>{
     // DeStructure chats, activeChat, userName, messages
     const { chats, activeChat, userName, messages} = props;
     // Continue Here
-    console.log(props);
+    const chat = chats && chats[activeChat];
+    // if 'chats' exits(&&) then look for 'activeChat'
+    //console.log(chat, userName, messages);
+    //console.log(props);
+    // Below Component to generate Messages
+    const renderMessages = () =>{
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[ index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+                </div>
+            )
+        })
+        console.log(keys);
+    }
+    renderMessages()
     return (
         <div>
             ChatFeed
